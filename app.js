5ed82194875022cee8725dea22e56a0edc840fed
Brandon Ellington <brandymathic@gmail.com>
2024-11-14 19:26:27 -0500

    Edited App.js and superheroes.php

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const httpRequest = new XMLHttpRequest();
-let url = "http://localhost/info2180-lab4/superheroes.php";
-httpRequest.open('GET', url, true);
-httpRequest.send();
-
+//Exercise 2 Implementation
 document.getElementById("search-btn").addEventListener("click", function() {
-  
-  httpRequest.onreadystatechange = function() {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-              const response = httpRequest.responseText;
-              alert(response); // Display the list of superheroes in an alert
-          } else {
-              alert("There was a problem with the request");
-          }
-      }
-  };
+    const httpRequest = new XMLHttpRequest();
+    let url = "http://localhost/info2180-lab4/superheroes.php";
+    
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                const response = httpRequest.responseText;
+                alert(response); 
+            } else {
+                alert("There was a problem with the request");
+            }
+        }
+    };
 
+    httpRequest.open('GET', url);
+    httpRequest.send();
 });
